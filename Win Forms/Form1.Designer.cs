fffab0fe00e1feac09f61ab714b105dec7909f9b
NekoLover <andry.i.andrushko@gmail.com>
2024-05-23 22:37:02 +0300

    Fixed GUI

--- Win Forms/Form1.Designer.cs
+++ Win Forms/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.startSimBtn = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LogTextBox = new System.Windows.Forms.TextBox();
             this.logLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,35 +41,38 @@
             this.label8 = new System.Windows.Forms.Label();
             this.VariationPercentTrack = new System.Windows.Forms.TrackBar();
             this.MutationRateTrack = new System.Windows.Forms.TrackBar();
-            this.SelectionAmountBox = new System.Windows.Forms.TextBox();
             this.PopulationSizeBox = new System.Windows.Forms.TextBox();
             this.MaxGenerationCBox = new System.Windows.Forms.TextBox();
-            this.MutationAmountBox = new System.Windows.Forms.TextBox();
             this.StagnationLimitBox = new System.Windows.Forms.TextBox();
             this.EliteCountBox = new System.Windows.Forms.TextBox();
             this.VariationPercentLable = new System.Windows.Forms.Label();
-            this.MuationRateLabel = new System.Windows.Forms.Label();
+            this.MutationRateLabel = new System.Windows.Forms.Label();
+            this.SelectionAmountTrack = new System.Windows.Forms.TrackBar();
+            this.SelectionAmountLabel = new System.Windows.Forms.Label();
+            this.MutationAmountBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.VariationPercentTrack)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.MutationRateTrack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectionAmountTrack)).BeginInit();
             this.SuspendLayout();
             // 
             // startSimBtn
             // 
             this.startSimBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.startSimBtn.Location = new System.Drawing.Point(266, 377);
+            this.startSimBtn.Location = new System.Drawing.Point(266, 393);
             this.startSimBtn.Name = "startSimBtn";
             this.startSimBtn.Size = new System.Drawing.Size(129, 61);
             this.startSimBtn.TabIndex = 0;
             this.startSimBtn.Text = "Start simulation";
             this.startSimBtn.UseVisualStyleBackColor = true;
+            this.startSimBtn.Click += new System.EventHandler(this.startSimBtn_Click);
             // 
-            // textBox1
+            // LogTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 25);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(236, 413);
-            this.textBox1.TabIndex = 1;
+            this.LogTextBox.Location = new System.Drawing.Point(12, 25);
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.Size = new System.Drawing.Size(236, 429);
+            this.LogTextBox.TabIndex = 1;
             // 
             // logLabel
             // 
@@ -110,7 +113,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(285, 181);
+            this.label4.Location = new System.Drawing.Point(285, 192);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(69, 13);
             this.label4.TabIndex = 6;
@@ -119,7 +122,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(285, 142);
+            this.label5.Location = new System.Drawing.Point(285, 153);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(86, 13);
             this.label5.TabIndex = 7;
@@ -128,7 +131,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(285, 229);
+            this.label6.Location = new System.Drawing.Point(285, 243);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(78, 13);
             this.label6.TabIndex = 8;
@@ -137,7 +140,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(285, 271);
+            this.label7.Location = new System.Drawing.Point(285, 282);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(57, 13);
             this.label7.TabIndex = 9;
@@ -146,7 +149,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(284, 310);
+            this.label8.Location = new System.Drawing.Point(285, 321);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(87, 13);
             this.label8.TabIndex = 10;
@@ -154,28 +157,23 @@
             // 
             // VariationPercentTrack
             // 
-            this.VariationPercentTrack.Location = new System.Drawing.Point(287, 326);
+            this.VariationPercentTrack.Location = new System.Drawing.Point(288, 337);
             this.VariationPercentTrack.Maximum = 100;
             this.VariationPercentTrack.Name = "VariationPercentTrack";
             this.VariationPercentTrack.Size = new System.Drawing.Size(104, 45);
             this.VariationPercentTrack.TabIndex = 11;
             this.VariationPercentTrack.Value = 35;
+            this.VariationPercentTrack.ValueChanged += new System.EventHandler(this.VariationPercentTrack_ValueChanged);
             // 
             // MutationRateTrack
             // 
-            this.MutationRateTrack.Location = new System.Drawing.Point(288, 197);
+            this.MutationRateTrack.Location = new System.Drawing.Point(287, 208);
             this.MutationRateTrack.Maximum = 100;
             this.MutationRateTrack.Name = "MutationRateTrack";
             this.MutationRateTrack.Size = new System.Drawing.Size(104, 45);
             this.MutationRateTrack.TabIndex = 12;
             this.MutationRateTrack.Value = 20;
-            // 
-            // SelectionAmountBox
-            // 
-            this.SelectionAmountBox.Location = new System.Drawing.Point(288, 119);
-            this.SelectionAmountBox.Name = "SelectionAmountBox";
-            this.SelectionAmountBox.Size = new System.Drawing.Size(100, 20);
-            this.SelectionAmountBox.TabIndex = 13;
+            this.MutationRateTrack.ValueChanged += new System.EventHandler(this.MutationRateTrack_ValueChanged);
             // 
             // PopulationSizeBox
             // 
@@ -183,6 +181,7 @@
             this.PopulationSizeBox.Name = "PopulationSizeBox";
             this.PopulationSizeBox.Size = new System.Drawing.Size(100, 20);
             this.PopulationSizeBox.TabIndex = 14;
+            this.PopulationSizeBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PopulationSizeBox_KeyPress);
             // 
             // MaxGenerationCBox
             // 
@@ -190,60 +189,83 @@
             this.MaxGenerationCBox.Name = "MaxGenerationCBox";
             this.MaxGenerationCBox.Size = new System.Drawing.Size(100, 20);
             this.MaxGenerationCBox.TabIndex = 15;
-            // 
-            // MutationAmountBox
-            // 
-            this.MutationAmountBox.Location = new System.Drawing.Point(288, 158);
-            this.MutationAmountBox.Name = "MutationAmountBox";
-            this.MutationAmountBox.Size = new System.Drawing.Size(100, 20);
-            this.MutationAmountBox.TabIndex = 16;
+            this.MaxGenerationCBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MaxGenerationCBox_KeyPress);
             // 
             // StagnationLimitBox
             // 
-            this.StagnationLimitBox.Location = new System.Drawing.Point(288, 248);
+            this.StagnationLimitBox.Location = new System.Drawing.Point(288, 259);
             this.StagnationLimitBox.Name = "StagnationLimitBox";
             this.StagnationLimitBox.Size = new System.Drawing.Size(100, 20);
             this.StagnationLimitBox.TabIndex = 17;
+            this.StagnationLimitBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.StagnationLimitBox_KeyPress);
             // 
             // EliteCountBox
             // 
-            this.EliteCountBox.Location = new System.Drawing.Point(288, 287);
+            this.EliteCountBox.Location = new System.Drawing.Point(287, 298);
             this.EliteCountBox.Name = "EliteCountBox";
             this.EliteCountBox.Size = new System.Drawing.Size(100, 20);
             this.EliteCountBox.TabIndex = 18;
+            this.EliteCountBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EliteCountBox_KeyPress);
             // 
             // VariationPercentLable
             // 
             this.VariationPercentLable.AutoSize = true;
-            this.VariationPercentLable.Location = new System.Drawing.Point(319, 346);
+            this.VariationPercentLable.Location = new System.Drawing.Point(319, 358);
             this.VariationPercentLable.Name = "VariationPercentLable";
             this.VariationPercentLable.Size = new System.Drawing.Size(27, 13);
             this.VariationPercentLable.TabIndex = 19;
             this.VariationPercentLable.Text = "35%";
             // 
-            // MuationRateLabel
+            // MutationRateLabel
             // 
-            this.MuationRateLabel.AutoSize = true;
-            this.MuationRateLabel.Location = new System.Drawing.Point(319, 217);
-            this.MuationRateLabel.Name = "MuationRateLabel";
-            this.MuationRateLabel.Size = new System.Drawing.Size(27, 13);
-            this.MuationRateLabel.TabIndex = 20;
-            this.MuationRateLabel.Text = "20%";
+            this.MutationRateLabel.AutoSize = true;
+            this.MutationRateLabel.Location = new System.Drawing.Point(319, 230);
+            this.MutationRateLabel.Name = "MutationRateLabel";
+            this.MutationRateLabel.Size = new System.Drawing.Size(27, 13);
+            this.MutationRateLabel.TabIndex = 20;
+            this.MutationRateLabel.Text = "20%";
+            // 
+            // SelectionAmountTrack
+            // 
+            this.SelectionAmountTrack.Location = new System.Drawing.Point(284, 118);
+            this.SelectionAmountTrack.Maximum = 100;
+            this.SelectionAmountTrack.Name = "SelectionAmountTrack";
+            this.SelectionAmountTrack.Size = new System.Drawing.Size(104, 45);
+            this.SelectionAmountTrack.TabIndex = 21;
+            this.SelectionAmountTrack.Value = 20;
+            this.SelectionAmountTrack.ValueChanged += new System.EventHandler(this.SelectionAmountTrack_ValueChanged);
+            // 
+            // SelectionAmountLabel
+            // 
+            this.SelectionAmountLabel.AutoSize = true;
+            this.SelectionAmountLabel.Location = new System.Drawing.Point(319, 140);
+            this.SelectionAmountLabel.Name = "SelectionAmountLabel";
+            this.SelectionAmountLabel.Size = new System.Drawing.Size(27, 13);
+            this.SelectionAmountLabel.TabIndex = 22;
+            this.SelectionAmountLabel.Text = "20%";
+            // 
+            // MutationAmountBox
+            // 
+            this.MutationAmountBox.Location = new System.Drawing.Point(288, 169);
+            this.MutationAmountBox.Name = "MutationAmountBox";
+            this.MutationAmountBox.Size = new System.Drawing.Size(100, 20);
+            this.MutationAmountBox.TabIndex = 23;
+            this.MutationAmountBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MutationAmountBox_KeyPress);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 450);
+            this.ClientSize = new System.Drawing.Size(421, 461);
+            this.Controls.Add(this.MutationAmountBox);
+            this.Controls.Add(this.SelectionAmountLabel);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.MuationRateLabel);
+            this.Controls.Add(this.MutationRateLabel);
             this.Controls.Add(this.VariationPercentLable);
             this.Controls.Add(this.EliteCountBox);
             this.Controls.Add(this.StagnationLimitBox);
-            this.Controls.Add(this.MutationAmountBox);
             this.Controls.Add(this.MaxGenerationCBox);
             this.Controls.Add(this.PopulationSizeBox);
-            this.Controls.Add(this.SelectionAmountBox);
             this.Controls.Add(this.MutationRateTrack);
             this.Controls.Add(this.VariationPercentTrack);
             this.Controls.Add(this.label8);
@@ -254,12 +276,14 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.logLabel);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LogTextBox);
             this.Controls.Add(this.startSimBtn);
+            this.Controls.Add(this.SelectionAmountTrack);
             this.Name = "Form1";
             this.Text = "Genetic algo";
             ((System.ComponentModel.ISupportInitialize)(this.VariationPercentTrack)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.MutationRateTrack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SelectionAmountTrack)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -268,7 +292,7 @@
         #endregion
 
         private System.Windows.Forms.Button startSimBtn;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox LogTextBox;
         private System.Windows.Forms.Label logLabel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -280,14 +304,15 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TrackBar VariationPercentTrack;
         private System.Windows.Forms.TrackBar MutationRateTrack;
-        private System.Windows.Forms.TextBox SelectionAmountBox;
         private System.Windows.Forms.TextBox PopulationSizeBox;
         private System.Windows.Forms.TextBox MaxGenerationCBox;
-        private System.Windows.Forms.TextBox MutationAmountBox;
         private System.Windows.Forms.TextBox StagnationLimitBox;
         private System.Windows.Forms.TextBox EliteCountBox;
         private System.Windows.Forms.Label VariationPercentLable;
-        private System.Windows.Forms.Label MuationRateLabel;
+        private System.Windows.Forms.Label MutationRateLabel;
+        private System.Windows.Forms.TrackBar SelectionAmountTrack;
+        private System.Windows.Forms.Label SelectionAmountLabel;
+        private System.Windows.Forms.TextBox MutationAmountBox;
     }
 }
 
---
+++ Win Forms/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows.Forms;
+using GeneticAlgo.Abstract;
+using GeneticAlgo;
+using GeneticAlgo.DTO;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Win_Forms_GUI
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random(Guid.NewGuid().GetHashCode());
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startSimBtn_Click(object sender, EventArgs e)
+        {
+            int population, maxGenerations, stagnationLimit, eliteCount;
+            double selectionAmount, mutationRate, mutationAmount, variationPercent;
+            try
+            {
+                population = Int32.Parse(PopulationSizeBox.Text);
+                maxGenerations = Int32.Parse(MaxGenerationCBox.Text);
+                selectionAmount = SelectionAmountTrack.Value * 0.01;
+                mutationRate = MutationRateTrack.Value * 0.01;
+                mutationAmount = Int32.Parse(MutationAmountBox.Text);
+                stagnationLimit = Int32.Parse(StagnationLimitBox.Text);
+                eliteCount = Int32.Parse(EliteCountBox.Text);
+                variationPercent = VariationPercentTrack.Value * 0.01;
+            }
+            catch
+            {
+                LogTextBox.AppendText("Wrong parameters");
+                return;
+            }
+
+
+            var parameters = new GA_Params(
+                population,
+                maxGenerations,
+                selectionAmount,
+                mutationRate,
+                mutationAmount,
+                100000,
+                stagnationLimit,
+                eliteCount
+                );
+            RunAlgorytm(parameters, variationPercent);
+        }
+
+        void RunAlgorytm(GA_Params parameters, double variationPercent)
+        {
+
+            int matrixSize = 10;
+
+            VariableRange[] xRanges = new VariableRange[matrixSize];
+            for (int i = 0; i < matrixSize; i++)
+            {
+                xRanges[i] = new VariableRange(-10000, 10000);
+            }
+
+            // Крок 1: Генерація матриці A
+            var A = GenerateNonSingularMatrix(matrixSize, matrixSize);
+
+            // Крок 2: Генерація розв'язку x0
+            var x0 = Vector<double>.Build.Dense(matrixSize, i => random.NextDouble() * 2000 - 1000);
+
+            // Крок 3: Формування матриці B
+            var B = A * x0;
+
+            // Крок 4: Вектор C
+            var C = Vector<double>.Build.Dense(matrixSize, i => SumColumn(A, i));
+
+            GeneticAlgoBase ga = new GA_WithRestrictions(A, B, C, xRanges, 0.0f, parameters);
+
+
+            ga.currentGenerationBest += (currentBest) =>
+            {
+                LogTextBox.AppendText("Current generation best: " + currentBest);
+            };
+
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var res = ga.Run();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            LogTextBox.AppendText(elapsedMs.ToString());
+            LogTextBox.AppendText(CalculateAverageDistance(res.BestResult.X, x0).ToString());
+        }
+
+        static double CalculateAverageDistance(Vector<double> vector1, Vector<double> vector2)
+        {
+            if (vector1.Count != vector2.Count)
+            {
+                throw new ArgumentException("Vectors must have the same dimensions.");
+            }
+
+            double totalDistance = 0;
+            int elementCount = vector1.Count;
+
+            for (int i = 0; i < vector1.Count; i++)
+            {
+                totalDistance += Math.Abs(vector1[i] - vector2[i]);
+            }
+
+            return totalDistance / elementCount;
+        }
+        Matrix<double> GenerateNonSingularMatrix(int rows, int cols)
+        {
+            Matrix<double> matrix;
+            do
+            {
+                matrix = Matrix<double>.Build.Dense(rows, cols, (i, j) => random.NextDouble() * 2000 - 1000);
+            }
+            while (matrix.Determinant() == 0);
+            return matrix;
+        }
+
+        double SumColumn(Matrix<double> matrix, int columnIndex)
+        {
+            double sum = 0;
+            for (int i = 0; i < matrix.RowCount; i++)
+            {
+                sum += matrix[i, columnIndex];
+            }
+            return sum;
+        }
+
+        private void PopulationSizeBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void MaxGenerationCBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void MutationAmountBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void StagnationLimitBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void EliteCountBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void SelectionAmountTrack_ValueChanged(object sender, EventArgs e)
+        {
+            SelectionAmountLabel.Text = SelectionAmountTrack.Value.ToString() + "%";
+        }
+
+        private void MutationRateTrack_ValueChanged(object sender, EventArgs e)
+        {
+            MutationRateLabel.Text = MutationRateTrack.Value.ToString() + "%";
+        }
+
+        private void VariationPercentTrack_ValueChanged(object sender, EventArgs e)
+        {
+            VariationPercentLable.Text = VariationPercentTrack.Value.ToString() + "%";
+        }
+    }
+}
